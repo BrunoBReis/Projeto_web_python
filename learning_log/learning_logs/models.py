577e9183f2e4f33793165d58970d1caab35a8261
BrunoBReis <brunobragancadosreis@gmail.com>
2023-07-18 12:15:15 -0300

    Criando os modelos do projeto

---
+++ learning_log/learning_logs/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Topic(models.Model):
+    """ Um assunto sobre o qual o usuário está aprendendo. """
+    
+    # texto armazenado no banco de dados
+    text = models.CharField(max_length=200)
+    
+    # registrará a data e hora 
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """ Devolve uma representação em string do modelo. """
+
+        return self.text
