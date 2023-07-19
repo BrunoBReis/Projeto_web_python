cd7cac9d3cf41374b61696be8c9fb3d69f5b80e2
BrunoBReis <brunobragancadosreis@gmail.com>
2023-07-19 08:10:24 -0300

    adicionando o app_name

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -3,6 +3,10 @@
 from django.urls import path
 from . import views
 
+# nome do aplicativo
+app_name = "learning_logs"
+
+# padrões de URL
 urlpatterns = [
     # página inicial 
     path('', views.index, name='index'),
